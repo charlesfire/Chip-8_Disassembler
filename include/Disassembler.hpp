5b54a70d495ecc4b46d449566f86a60faa536ddd
Charlesfire <charlesfire2@hotmail.com>
2015-10-25 10:34:20 -0400

    Ajout d'un exemple de base

--- include/Disassembler.hpp
+++ include/Disassembler.hpp
@@ -1,6 +1,11 @@
 #ifndef DISASSEMBLER_HPP
 #define DISASSEMBLER_HPP
 
+#include <array>
+#include <functional>
+#include <ostream>
+#include <string>
+
 class Disassembler
 {
     public:
@@ -9,6 +14,8 @@ class Disassembler
         bool LoadAssembly(const std::string& path);
         void Disassemble(std::ostream& out);
     private:
+        std::array<std::function<void(Disassembler*, std::ostream&, uint8_t, uint8_t, uint8_t)>, 35> mapper;
+        void Sys0xNNN(std::ostream& out, uint8_t b1, uint8_t b2, uint8_t b3);
 };
 
 #endif // DISASSEMBLER_HPP
--- src/Disassembler.cpp
+++ src/Disassembler.cpp
@@ -1,11 +1,27 @@
 #include "Disassembler.hpp"
+#include <functional>
 
 Disassembler::Disassembler()
 {
-    //ctor
+    mapper[0] = &Disassembler::Sys0xNNN;
 }
 
 Disassembler::~Disassembler()
 {
-    //dtor
+
+}
+
+bool Disassembler::LoadAssembly(const std::string& path)
+{
+
+}
+
+void Disassembler::Disassemble(std::ostream& out)
+{
+
+}
+
+void Disassembler::Sys0xNNN(std::ostream& out, uint8_t b1, uint8_t b2, uint8_t b3)
+{
+    out << "SYS 0x" << std::hex << b3 << b2 << b1;
 }
